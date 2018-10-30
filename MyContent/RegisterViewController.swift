73d93192219fbb2048b472c335a7ac065a1d0b1a
Thekob <thekob.mx@gmail.com>
2018-10-30 11:52:30 +0700

    Design UI Register

---
+++ MyContent/RegisterViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterViewController.swift
+//  MyContent
+//
+//  Created by kotchaphan on 30/10/2561 BE.
+//  Copyright © 2561 Thanapat.P. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
