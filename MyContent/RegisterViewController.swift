431fb7aaacbabd8a1a06cc2516ea54f8568b22cf
Thekob <thekob.mx@gmail.com>
2018-10-30 13:17:01 +0700

    Check Space OK

--- MyContent/RegisterViewController.swift
+++ MyContent/RegisterViewController.swift
@@ -9,14 +9,81 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    //Explicit
+    var nameString: String = ""
+    var userString: String = ""
+    var passwordString: String = ""
+    
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func uploadButton(_ sender: Any) {
+        
+        nameString = nameTextField.text!
+        userString = userTextField.text!
+        passwordString = passwordTextField.text!
+        
+        showLogString()
+        
+        if checkString() {
+//            Have Space
+            
+            
+            
+        } else {
+//            No Space
+            
+            
+            
+        }
+        
+        
+        
+    }//upload
+    
+    
+    func checkString() -> Bool {
+        
+        
+        var result: Bool?
+        
+        if (nameString.count == 0) || (userString.count == 0) || (passwordString.count == 0) {
+            result = true
+        } else{
+            result = false
+        }
+        
+        
+        print("checkString ==> \(result!)")
+        
+        return result!
         
     }
     
-
     
-
-}
+    func showLogString() -> Void {
+        print("name ==> \(nameString)")
+        print("user ==> \(userString)")
+        print("password ==> \(passwordString)")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    } // viewDidLoad
+    
+    
+    
+    
+} //Main Class
